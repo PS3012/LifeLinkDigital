129b67e89cb7de7d9935628a25851263d99cda8b
Gaurav <115947461+gauravv0207@users.noreply.github.com>
2024-02-09 12:26:33 +0530

    breadcrump removed and about page is in updating phase

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,7 @@
 import BreadCrumb from "../components/BreadCrumb";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck, faUsers } from "@fortawesome/free-solid-svg-icons";
-import {
-  faFacebookF,
-  faInstagram,
-  faLinkedinIn,
-  faTwitter,
-} from "@fortawesome/free-brands-svg-icons";
+import { faFacebookF, faInstagram, faLinkedinIn, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 function About() {
   return (
@@ -24,18 +19,85 @@ function About() {
             </div>
             <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
               {/* <div className="btn btn-sm border rounded-pill text-primary px-3 mb-3">About Us</div> */}
-              <h3 className="mb-4">
-                Welcome to Life Link Digital Private Limited – Your Gateway to
-                Technological Excellence!
-              </h3>
+              <h3 className="mb-4">Lifelink Digital: Streamlining GxP Compliance for Global Pharma</h3>
               <p className="mb-4">
-                At Life Link Digital, we are pioneers in harnessing the power of
-                cutting-edge technology to transform businesses and elevate
-                digital experiences. As a leading player in the digital
-                landscape, we specialize in providing innovative solutions that
-                seamlessly integrate with the dynamic needs of today's
-                fast-paced world.
+                Lifelink Digital is a leading global provider of GxP software solutions, empowering pharmaceutical and
+                life sciences companies to achieve and maintain compliance efficiently. With a deep understanding of GxP
+                regulations and industry best practices, we offer a comprehensive suite of software solutions designed
+                to streamline:
               </p>
+              <div className="content_container">
+                <ul className="list-group">
+                  <li className="list-group-item">
+                    <b>Data integrity and quality management</b>
+                    Ensure data accuracy and traceability across your entire workflow with our data management
+                    solutions.
+                  </li>
+                  <li className="list-group-item">
+                    <b> Document management and control: </b>Simplify document creation, review, approval, and version
+                    control with our intuitive document management system.
+                  </li>
+                  <li className="list-group-item">
+                    <b> Regulatory compliance: </b> Stay ahead of regulatory changes with our automated solutions for
+                    risk management, audit trails, and electronic signatures.
+                  </li>
+                  <li className="list-group-item">
+                    <b> Quality management systems: </b>Implement and manage your QMS effectively with our integrated
+                    quality management software.
+                  </li>
+                  <li className="list-group-item">
+                    <b> Collaboration and communication: </b>Foster seamless collaboration across teams and departments
+                    with our secure communication and workflow management tools.
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container-fluid py-5">
+        <div className="container">
+          <div className="row g-5 align-items-center">
+            <div className="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
+              {/* <div className="btn btn-sm border rounded-pill text-primary px-3 mb-3">About Us</div> */}
+              <h3 className="mb-4">By partnering with Lifelink Digital, you can:</h3>
+              <p className="mb-4">
+                Lifelink Digital is a leading global provider of GxP software solutions, empowering pharmaceutical and
+                life sciences companies to achieve and maintain compliance efficiently. With a deep understanding of GxP
+                regulations and industry best practices, we offer a comprehensive suite of software solutions designed
+                to streamline:
+              </p>
+              <div className="content_container">
+                <ul className="list-group">
+                  <li className="list-group-item">
+                    <b> Reduce costs and improve efficiency </b>
+                    Eliminate manual processes and centralize data management for significant cost savings and increased
+                    operational efficiency.
+                  </li>
+                  <li className="list-group-item">
+                    <b> Minimize risk and ensure compliance: </b>Mitigate regulatory risks and ensure consistent
+                    compliance with our validated and configurable software solutions.
+                  </li>
+                  <li className="list-group-item">
+                    <b>  Improve data quality and visibility: </b> Gain complete visibility into your data and
+                    processes for better decision-making and improved data integrity.
+                  </li>
+                  <li className="list-group-item">
+                    <b> Quality management systems: </b>Implement and manage your QMS effectively with our integrated
+                    quality management software.
+                  </li>
+                  <li className="list-group-item">
+                    <b> Collaboration and communication: </b>Foster seamless collaboration across teams and departments
+                    with our secure communication and workflow management tools.
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+              <div className="abouts">
+                <img className="img-fluid" src="/gradient.jpg" />
+              </div>
             </div>
           </div>
         </div>
@@ -62,9 +124,8 @@ function About() {
                   <div className="software">Software Development</div>
                   <div className="software-pera">
                     {" "}
-                    Our team of seasoned developers is adept at creating
-                    scalable, efficient, and secure software applications
-                    tailored to your specifications.
+                    Our team of seasoned developers is adept at creating scalable, efficient, and secure software
+                    applications tailored to your specifications.
                   </div>
                 </div>
               </div>
@@ -80,7 +141,8 @@ function About() {
                   <div className="software">Web Design</div>
                   <div className=" software-pera">
                     {" "}
-                    Our web design services focus on creating responsive, user-friendly,robust backends with the latest web technologies, and visually captivating websites.
+                    Our web design services focus on creating responsive, user-friendly,robust backends with the latest
+                    web technologies, and visually captivating websites.
                   </div>
                 </div>
               </div>{" "}
@@ -96,7 +158,8 @@ function About() {
                   <div className="software">Pharma Digitization</div>
                   <div className="software-pera">
                     {" "}
-                    The pharma industry faces a paradigm shift, which paves the way for more efficient, patient-centric, and data-driven approaches.
+                    The pharma industry faces a paradigm shift, which paves the way for more efficient, patient-centric,
+                    and data-driven approaches.
                   </div>
                 </div>
               </div>{" "}
@@ -112,7 +175,8 @@ function About() {
                   <div className="software">Artificial Intelligence</div>
                   <div className=" software-pera">
                     {" "}
-                    Embrace the future with AI solutions tailored to enhance your business operations. From machine learning algorithms to natural language processing.
+                    Embrace the future with AI solutions tailored to enhance your business operations. From machine
+                    learning algorithms to natural language processing.
                   </div>
                 </div>
               </div>
@@ -121,14 +185,11 @@ function About() {
         </div>
       </div>
 
-      <div
-        className="col-lg-6 align-self-end text-center text-md-end wow fadeIn"
-        data-wow-delay="0.5s"
-      ></div>
+      <div className="col-lg-6 align-self-end text-center text-md-end wow fadeIn" data-wow-delay="0.5s"></div>
       {/* <!-- Feature End --> */}
 
       {/* <!-- Team Start --> */}
-      <div className="container-fluid bg-light py-5">
+      {/* <div className="container-fluid bg-light py-5">
         <div className="container py-5">
           <div className="row g-5 align-items-center">
             <div className="col-lg-5 wow fadeIn" data-wow-delay="0.1s">
@@ -260,7 +321,7 @@ function About() {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
       {/* <!-- Team End --> */}
     </>
   );
